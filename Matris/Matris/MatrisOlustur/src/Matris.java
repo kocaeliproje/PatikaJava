512245b96f27b9b128e35aa1010c469c4c403854
Mehmet Aydın <54308525+kocaeliproje@users.noreply.github.com>
2023-12-06 22:53:31 +0300

    Add files via upload
    Kullanıcıdan Matris bilgilerini alıp ekrana yazdıran program

---
+++ Matris/Matris/MatrisOlustur/src/Matris.java
@@ -0,0 +1,36 @@
+//Kullanıcıdan Matris bilgilerini alıp ekrana yazdıran program
+
+import java.util.Scanner;
+
+public class Matris {
+
+    public static  void  main (String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Matris X boyutunu giriniz: ");
+        int boyutX = inp.nextInt();
+        System.out.print("Matris Y boyutunu giriniz: ");
+        int boyutY = inp.nextInt();
+        System.out.print(boyutX + " x " + boyutY + " Boyutlu Matrisin 1. satırını giriniz: ");
+
+        int matris[][] = new int[boyutX][boyutX];
+
+        for (int i = 0; i < matris.length; i++) {
+
+            for (int j = 0; j < matris[i].length; j++) {
+
+                matris[i][j] = inp.nextInt();
+
+            }
+        }
+
+        for (int i = 0; i < matris.length; i++) {
+
+            System.out.println(" ");
+            for (int j = 0; j < matris[i].length; j++) {
+                System.out.print(matris[i][j]);
+
+            }
+        }
+    }
+
+}
